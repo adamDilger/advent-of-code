136f9a5106f0cb76ba9e3eb3eff862fc979a0853
Adam Dilger <adamdilger@Adams-MacBook-Air.local>
2022-12-06 21:17:33 +1100

    06

---
+++ 2022/06/06.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"io/ioutil"
+)
+
+func main() {
+	input, err := ioutil.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	res := make(map[rune]bool)
+
+MainLoop:
+	for i := 0; i < len(input); i++ {
+		println(string(input[i]))
+
+		for j := 0; j < 4; j++ {
+			print(string(input[i+j]))
+
+			if res[rune(input[i+j])] {
+				res = make(map[rune]bool)
+				continue MainLoop
+			}
+
+			res[rune(input[i+j])] = true
+		}
+
+		println("MATCH", i+4)
+		break
+		// if i == 2 {
+		// 	break
+		// }
+	}
+}
