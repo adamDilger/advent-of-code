46c49fe993c14b728477ce7d50a5e87eda2b1d7e
Adam Dilger <adamdilger1@gmail.com>
2023-12-01 23:13:01 +1100

    must have been my last attempt....

--- 2022/13/13.go
+++ 2022/13/13.go
@@ -3,30 +3,32 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"os"
 	"strconv"
+	"strings"
 )
 
 type Item struct {
-	List    bool
+	IsList  bool
 	Val     int
 	ListVal []*Item
 }
 
-func (i *Item) printAll(newline bool) {
-	for _, c := range i.ListVal {
-		if c.List {
-			fmt.Print("[")
-			c.printAll(false)
-			fmt.Print("]")
-		} else {
-			fmt.Print(c.Val, ",")
-		}
+func (i *Item) String() string {
+	if !i.IsList {
+		return fmt.Sprintf("%d", i.Val)
 	}
 
-	if newline {
-		fmt.Println()
+	out := "["
+	for idx, v := range i.ListVal {
+		if idx > 0 {
+			out += ","
+		}
+		out += fmt.Sprintf("%v", v.String())
 	}
+
+	out += "]"
+
+	return out
 }
 
 func buildItems(nodes []Node, currItemsPtr []*Item) {
@@ -42,7 +44,7 @@ func buildItems(nodes []Node, currItemsPtr []*Item) {
 
 	if node.start {
 		// add an item to the list
-		i := Item{List: true}
+		i := Item{IsList: true}
 
 		// append to current item
 		currItemsPtr[lastIndex].ListVal = append(currItemsPtr[lastIndex].ListVal, &i)
@@ -96,30 +98,54 @@ func parseLine(line string) []Node {
 }
 
 func main() {
-	file, err := os.Open("test.txt")
-	if err != nil {
-		panic(err)
-	}
+	// file, err := os.Open("test.txt")
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	scanner := bufio.NewScanner(file)
+	scanner := bufio.NewScanner(
+		strings.NewReader("[[1],[2,3,4]]\n[[1],4]\n"),
+	)
+
+	count := 1
 
 	for scanner.Scan() {
+		fmt.Printf("== Pair %d ==\n", count)
+
 		line1 := scanner.Text()
 		scanner.Scan()
 		line2 := scanner.Text()
 
-		fmt.Println(line1)
-		fmt.Println(line2)
-
-		ind1 := &Item{List: true}
+		ind1 := &Item{IsList: true}
 		buildItems(parseLine(line1), []*Item{ind1})
-		ind1.printAll(true)
 
-		ind2 := &Item{List: true}
+		ind2 := &Item{IsList: true}
 		buildItems(parseLine(line2), []*Item{ind2})
-		ind2.printAll(true)
+
+		fmt.Printf("- Compare %s vs %s\n", line1, line2)
+
+		fmt.Printf("%v\n", ind1)
+		fmt.Printf("%v\n", ind2)
+
+		indexes := compare(ind1, ind2, 0, []int{})
+		fmt.Println(indexes)
 
 		scanner.Scan()
-		fmt.Println("------------------------------")
+		count++
+		println()
+		break
 	}
+}
+
+func compare(i1, i2 *Item, curIndex int, idx []int) []int {
+	if !i1.IsList && !i2.IsList {
+		// compare the values
+		if i1.Val > i2.Val {
+			idx = append(idx, curIndex)
+		}
+
+		return idx
+	}
+
+	return idx
 }
