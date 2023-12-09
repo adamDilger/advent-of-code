0c8ce332b449e92a031092bea4945a63b208ca5b
Adam Dilger <adamdilger1@gmail.com>
2023-12-09 22:39:57 +1100

    06 02

--- 2023/06/main.go
+++ 2023/06/main.go
@@ -18,62 +18,58 @@ func main() {
 		panic(err)
 	}
 
-	races := parseRaces(file)
-
 	total := 1
 
-	for _, race := range races {
-		fmt.Println(race)
+	race := parseRaces(file)
+	fmt.Println(race)
 
-		// dist = (raceTime - btnTime) * btnTime
+	// dist = (raceTime - btnTime) * btnTime
 
-		isEven := race.time%2 == 0
+	isEven := race.time%2 == 0
 
-		mid := (race.time / 2) + 1
+	mid := (race.time / 2) + 1
 
-		for btnTime := mid; btnTime <= race.time; btnTime++ {
-			dist := (race.time - btnTime) * btnTime
-			// fmt.Printf("btn: %d | dist: %d\n", btnTime, dist)
+	for btnTime := mid; btnTime <= race.time; btnTime++ {
+		dist := (race.time - btnTime) * btnTime
+		// fmt.Printf("btn: %d | dist: %d\n", btnTime, dist)
 
-			if dist <= race.record {
-				var count int
-				if isEven {
-					count = ((btnTime - mid) * 2) + 1
-				} else {
-					count = ((btnTime - mid) * 2)
-				}
-
-				total *= count
-				fmt.Printf("FOUND: %d\n", count)
-				break
+		if dist <= race.record {
+			var count int
+			if isEven {
+				count = ((btnTime - mid) * 2) + 1
+			} else {
+				count = ((btnTime - mid) * 2)
 			}
+
+			total *= count
+			fmt.Printf("FOUND: %d\n", count)
+			break
 		}
 	}
 
 	fmt.Println(total)
 }
 
-func parseRaces(f *os.File) []Race {
-	var races []Race
+func parseRaces(f *os.File) Race {
+	var race Race
 
 	sc := bufio.NewScanner(f)
 	sc.Scan()
-	for _, time := range strings.Fields(sc.Text())[1:] {
-		if timeNum, err := strconv.Atoi(time); err == nil {
-			races = append(races, Race{time: timeNum})
-		} else {
-			panic(err)
-		}
+	_, line, _ := strings.Cut(sc.Text(), "Time: ")
+
+	if timeNum, err := strconv.Atoi(strings.ReplaceAll(line, " ", "")); err == nil {
+		race.time = timeNum
+	} else {
+		panic(err)
 	}
 
 	sc.Scan()
-	for i, dist := range strings.Fields(sc.Text())[1:] {
-		if distNum, err := strconv.Atoi(dist); err == nil {
-			races[i].record = distNum
-		} else {
-			panic(err)
-		}
+	_, line, _ = strings.Cut(sc.Text(), "Distance: ")
+	if distNum, err := strconv.Atoi(strings.ReplaceAll(line, " ", "")); err == nil {
+		race.record = distNum
+	} else {
+		panic(err)
 	}
 
-	return races
+	return race
 }
