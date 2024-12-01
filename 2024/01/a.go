110c6926a9df03056f26f6e9d6aac20ac1545c3c
Adam Dilger <adamdilger1@gmail.com>
2024-12-01 22:59:09 +1100

    2024 01 a

---
+++ 2024/01/a.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"slices"
+)
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	run(file)
+}
+
+func run(f io.Reader) int {
+	sc := bufio.NewScanner(f)
+
+	var l1, l2 []int
+
+	for sc.Scan() {
+		t := sc.Text()
+
+		if sc.Err() != nil {
+			panic(sc.Err())
+		}
+
+		var left, right int
+		fmt.Sscanf(t, "%d   %d\n", &left, &right)
+		l1 = append(l1, left)
+		l2 = append(l2, right)
+	}
+
+	slices.Sort(l1)
+	slices.Sort(l2)
+	// fmt.Println(l1, l2)
+
+	sum := 0
+	for i := 0; i < len(l1); i++ {
+		l, r := l1[i], l2[i]
+
+		// fmt.Println(l, r, abs(l-r))
+		sum += abs(l - r)
+	}
+
+	fmt.Println(sum)
+	return sum
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return x * -1
+	}
+
+	return x
+}
